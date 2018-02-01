ef52f726723d561d3d894d19599ce9d16b846be4
timster2938 <ruggt001@icloud.com>
2018-01-31 22:28:14 -0500

    complete spotify

--- liri.js
+++ liri.js
@@ -6,8 +6,8 @@ var Spotify = require('node-spotify-api');
 var fs = require("fs");
 
 var inputString = process.argv;
-navigateToLIRI(inputString[2]);
-
+navigateToLIRI(inputString[2]); // use the first argument to decide which menu item to go to
+// MENU ITEMS
 function navigateToLIRI(action) {
     switch (action) {
         case "my-tweets":
@@ -48,43 +48,47 @@ function getTweets(){
 }
 
 function getSong() {
-console.log("spotify-this-song");
     // if no song title was input, use The Sign else parse input together if without quotes
     var lookupSongValue = "";
     if (lookupValue()=="ErrNoLookup") {
-        lookupSongValue = "The Sign";   
+        lookupSongValue = "the sign";   
     } else { 
         lookupSongValue = lookupValue();
     };
     // pull back and output song information
     var spotify = new Spotify(keys.spotify); // using keys.js sends the two spotify keys to create a Spotify object
-    spotify.search({type:'track', query:'Aftertaste'}, function(err, data) {
+    // spotify.search({type:'track', query:'Aftertaste'}, function(err, data) {
+    spotify.search({type:'track', query:lookupSongValue}, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-//Save all arrays to variables
-//Loop through for albums
-//Loop through for artists on album
-//output Song - Artist(s) - Album - Preview URL 
-        // var songData = JSON.stringify(data);
-        console.log("--------------------------------------------ITEMS");
-        console.log(data.tracks.items[0].name);
-        console.log(data.tracks.items[0].preview_url);
-        console.log("--------------------------------------------ALBUM");
-        console.log(data.tracks.items[0].album.album_type);
-        console.log(data.tracks.items[0].album.name);
-        console.log("--------------------------------------------ARTISTS");
-        console.log(data.tracks.items[0].artists[0].name);
-        // console.log(data.tracks.items);
-        // console.log("Artist(s): " + artist);
-        // console.log("Song: " + song);
-        // console.log("Preview: " + preview);
-        // console.log("Album: " + album);       
+        var spotifyItems = data.tracks.items; // to simplify values in console logs
+        for (var i=0; i < spotifyItems.length; i++) {
+            // the API treats the search as a contains returning records the name is part of and even though track is searched
+            // a record is returned if the album also matches song name
+            if (spotifyItems[i].album.album_type == "album" && spotifyItems[i].name.toLowerCase() == lookupSongValue) {
+            // multiple results can be found for one song - this outputs all albums    
+                console.log("-------------------------------------------------------------------");
+                console.log("Song: " + spotifyItems[i].name 
+                        + "\nAlbum: " + spotifyItems[i].album.name 
+                        + "\nPreview: " + spotifyItems[i].preview_url);
+                    var spotifyArtists = "";
+                    // multiple artists be named
+                    for (var j=0; j < spotifyItems[i].artists.length; j++) {
+                        if (j > 0) {
+                             spotifyArtists += ", "; // place a comma before the next value
+                        };       
+                        spotifyArtists += spotifyItems[i].artists[j].name;        
+                    }
+                    console.log("Artist(s): " + spotifyArtists); // output all artis concatenated above
+            }
+        }
     });
 }
 
 function lookupValue() {
 //concatenate the other process args together into one string
+//a parameter can be in quotes or a string out of quotes
     var lookup = "";
     for (i = 3; i < inputString.length; i++) {
         lookup += inputString[i] + " "; 
@@ -92,7 +96,7 @@ function lookupValue() {
     if (lookup == "") {
         return "ErrNoLookup";
     } else {
-        return lookup.trim(); // remove trailing space
+        return lookup.trim().toLowerCase(); // remove trailing space and change to all lower case
     }   
 }
 
